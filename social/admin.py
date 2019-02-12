10fdfc7209a44d6613f0d9345bb5f625d9b0479d
KevinCastPer <kecasper@gmail.com>
2019-02-12 18:24:23 +0100

    Crear el grupo para seleccionar permisos, y crear el usuario que le asigna el permiso de grupo. views solo le da el permiso de lectura si accede un usuario que intenta modificar o crear redes sociales

--- social/admin.py
+++ social/admin.py
@@ -5,4 +5,11 @@ from .models import Link
 class LinkAdmin(admin.ModelAdmin):
     readonly_fields = ('created', 'updated')
 
+    # si alquien se llama el grupo personal en el administrador que solo da permiso: 
+    def get_readonly_fields(self, request, obj=None):
+        if request.user.groups.filter(name="Personal").exists():
+            return ('key', 'name')
+        else:
+            return ('created', 'updated')
+
 admin.site.register(Link, LinkAdmin)
